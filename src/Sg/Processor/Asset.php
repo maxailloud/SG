eaaa3affbf66702ddcd56a9b96a4178eb1305f87
Maxime AILLOUD <maxime.ailloud@gmail.com>
2012-03-22 23:38:08 +0100

    Gestion des assets en global avec assetic

--- src/Sg/Processor/Asset.php
+++ src/Sg/Processor/Asset.php
@@ -2,7 +2,10 @@
 
 namespace Sg\Processor;
 
-use Symfony\Component;
+use Assetic\AssetWriter;
+use Assetic\AssetManager;
+use Assetic\Asset\FileAsset;
+use Assetic\Asset\GlobAsset;
 
 /**
  * Asset processor class.
@@ -44,9 +47,52 @@ class Asset extends \Sg\Outputter
      */
     public function process($sourceDirectory, $destinationDirectory)
     {
-//        use Assetic\AssetWriter;
-//
-//        $writer = new AssetWriter('/path/to/web');
-//        $writer->writeManagerAssets($am);
+        $stylesheetSourceDirectory = $sourceDirectory . DIRECTORY_SEPARATOR . 'asset' . DIRECTORY_SEPARATOR . 'css';
+        $this->checkDirectory($stylesheetSourceDirectory);
+
+        $stylesheetDestinationDirectory = $destinationDirectory . DIRECTORY_SEPARATOR . 'css';
+        $this->checkDirectory($stylesheetDestinationDirectory, true);
+
+        $stylesheetGlobAsset = new GlobAsset($stylesheetSourceDirectory . DIRECTORY_SEPARATOR . '*');
+        echo "<pre>";
+        var_dump($stylesheetGlobAsset->dump());
+        echo "</pre>" . PHP_EOL;
+        $stylesheetAssetManager = new AssetManager();
+        $stylesheetAssetManager->set('css', $stylesheetGlobAsset);
+
+        $stylesheetWriter = new AssetWriter($stylesheetDestinationDirectory);
+        $stylesheetWriter->writeManagerAssets($stylesheetAssetManager);
+
+        $this->writeResult(self::OUTPUT_OK, 'Asset file of the site treated.');
+    }
+
+
+    /**
+     * @param string $directory
+     * @param bool $writable
+     * @return \Sg\Processor\Asset
+     * @throws \Exception
+     */
+    public function checkDirectory($directory, $writable = false)
+    {
+        if(false === is_dir($directory))
+        {
+            throw new \Exception(sprintf("The directory '%s' doesn't exist.", $directory));
+        }
+
+        if(false === is_readable($directory))
+        {
+            throw new \Exception(sprintf("The directory '%s' cannot be read.", $directory));
+        }
+
+        if(true === $writable)
+        {
+            if(false === is_writable($directory))
+            {
+                throw new \Exception(sprintf("You don't have the right permission to write into the directory '%s'.", $this->destinationDirectory));
+            }
+        }
+
+        return $this;
     }
 }
